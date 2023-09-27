66a91f5fcdf102753f2f2a13acce8dd15c152ce6
diaken2 <kenansalahov111@gmail.com>
2023-09-28 03:09:43 +0500

    added ESlint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>X-Progers</h1>
-      <h2>New-Progers</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>X-Progers</h1>
+            <h2>New-Progers</h2>
+        </div>
+    )
 }
 
-export default App;
+export default App
