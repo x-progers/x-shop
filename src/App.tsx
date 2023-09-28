1da85f8e3f47990230f4ad5b7fa8021fa291aec3
diaken2 <kenansalahov111@gmail.com>
2023-09-29 02:25:58 +0500

    added i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import './App.css';
-
+import { useTranslation } from "react-i18next";
 function App() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language: string | undefined) => {
+    i18n.changeLanguage(language);
+  };
   return (
     <div className="App">
+       <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("ru")}>RU</button>
+      <hr />
+      <div><h1>{t("title")}</h1></div>
       <h1>X-Progers</h1>
       <h2>New-Progers</h2>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "./i18n";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
     <App />
+    </Suspense>
   </React.StrictMode>
 );
 
