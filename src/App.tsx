07d1a4982e500e5104c53f610d565ab14d00de98
Kirilll <109998010+kostiukirill@users.noreply.github.com>
2023-09-30 18:36:07 +0300

    fix(absolutePaths): fixed error 'Module not found'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import { TestComponent } from '@components';
+import { Test } from '@constants';
 
+
+ 
 
 function App() {
   return (
     <div className="App">
       <TestComponent/>
       <h1>X-Progers</h1>
-      <h2>New-Progers</h2>
+      <h2>{Test}</h2>
     </div>
   );
 }
