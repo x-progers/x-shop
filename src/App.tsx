46c1a2d036050c5041a2e915de0c1d3ca7a77d70
KenanSalahov <107970623+diaken2@users.noreply.github.com>
2023-10-04 23:46:38 +0500

    Update App.tsx
    added useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-
-import './App.css';
+import React, { useCallback } from "react";
+import "./App.css";
 import { useTranslation } from "react-i18next";
 function App() {
   const { t, i18n } = useTranslation();
-  const changeLanguage = (language: string | undefined) => {
-    i18n.changeLanguage(language);
-  };
+  const changeLanguage = useCallback(
+    (language: string | undefined) => () => i18n.changeLanguage(language),
+    [],
+  );
   return (
     <div className="App">
-       <button onClick={() => changeLanguage("en")}>EN</button>
-      <button onClick={() => changeLanguage("ru")}>RU</button>
+      <button onClick={changeLanguage("en")}>EN</button>
+      <button onClick={changeLanguage("ru")}>RU</button>
       <hr />
-      <div><h1>{t("title")}</h1></div>
+      <div>
+        <h1>{t("title")}</h1>
+      </div>
       <h1>X-Progers</h1>
       <h2>New-Progers</h2>
     </div>
